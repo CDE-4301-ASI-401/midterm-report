bca4e94dbacf3796727a0be94b0e59c6cb143733
Foo Shu Hui <97970820+shuhuiii@users.noreply.github.com>
2024-11-15 19:36:11 +0800

    add left nav

--- components/left-nav-bar/left-nav-bar.js
+++
@@ -1,8 +0,0 @@
-const sidebar = document.getElementById('sidebar');
-const toggleButton = document.getElementById('toggleButton');
-const mainContent = document.querySelector('.main-content');
-
-toggleButton.addEventListener('click', () => {
-  sidebar.classList.toggle('collapsed');
-  mainContent.classList.toggle('collapsed');
-});
