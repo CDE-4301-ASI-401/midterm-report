ae29f9de142825b07ee9f1ccdb11f53dc85777c6
Foo Shu Hui <97970820+shuhuiii@users.noreply.github.com>
2025-03-30 21:29:30 +0800

    section 7 image and table

---
+++ components/decision-matrix-pillar/decision-matrix-pillar.js
@@ -0,0 +1,54 @@
+new gridjs.Grid({
+  columns: ["Solution", "A", "B", "C"],
+  data: [
+    ["Loop avoidance?", "No", "Yes", "Yes"],
+    ["Maintains course?", "Yes", "Yes", "Yes"],
+    ["Avoids collision?", "Yes", "Yes", "Yes"],
+    ["Predictable flight path?", "No", "No", "No"],
+  ],
+}).render(document.getElementById("decison-matrix-pillar"));
+
+class TableComponent extends HTMLElement {
+  static get observedAttributes() {
+    return ["subtitle"];
+  }
+
+  constructor() {
+    super();
+    this.attachShadow({ mode: "open" });
+  }
+
+  connectedCallback() {
+    this.render();
+  }
+
+  attributeChangedCallback(name, _, newValue) {
+    this[name] = newValue;
+  }
+
+  render() {
+    const div = document.createElement("div");
+    div.innerHTML = `
+    <sub>${this.subtitle}</sub>
+    <slot></slot>
+    <style>
+      :host {
+        display: block;
+        text-align: center;
+        word-wrap: break-word;
+        overflow: hidden;
+        white-space: normal;
+      }
+
+      sub {
+        font-size: 1rem;
+        font-style: italic;
+      }
+    </style>
+  `;
+
+    this.shadowRoot.appendChild(div);
+  }
+}
+
+customElements.define("decison-matrix-pillar", TableComponent);
